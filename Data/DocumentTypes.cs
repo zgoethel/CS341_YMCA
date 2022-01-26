3829e65d4327c106efdb2bf225206606ee15098c
Zach Goethel <13502112+zgoethel@users.noreply.github.com>
2022-01-26 13:22:32 -0600

    Initial commit

---
+++ Data/DocumentTypes.cs
@@ -0,0 +1,43 @@
+﻿namespace CS341_YMCA.Data
+{
+    // Models bound to the fields required by user SQL procedures
+    #region SiteUserActionModels
+
+    public class SiteUserAuthenticateRequest
+    {
+        public string Email { get; set; } = "";
+        public string PasswordHash { get; set; } = "";
+    }
+
+    public class SiteUserRegisterRequest
+    {
+        public string FirstName { get; set; } = "";
+        public string? LastName { get; set; }
+        public string Email { get; set; } = "";
+        public bool IsAdmin { get; set; } = false;
+    }
+
+    public class SiteUserRegisterResult
+    {
+        public int Id { get; set; }
+        public Guid ResetToken { get; set; }
+    }
+
+    public class UserRequestResetRequest
+    {
+        public string Email { get; set; } = "";
+    }
+
+    public class UserRequestResetResult
+    {
+        public Guid ResetToken { get; set; }
+    }
+
+    public class SiteUserResetPasswordRequest
+    {
+        public Guid ResetToken { get; set; }
+        public string PasswordHash { get; set; } = "";
+    }
+
+    #endregion
+}
---
+++ Program.cs
@@ -0,0 +1,27 @@
+using CS341_YMCA.Data;
+
+var Builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+Builder.Services.AddRazorPages();
+Builder.Services.AddServerSideBlazor();
+Builder.Services.AddTransient<Database>();
+
+var App = Builder.Build();
+
+// Configure the HTTP request pipeline.
+if (!App.Environment.IsDevelopment())
+{
+    App.UseExceptionHandler("/Error");
+    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+    App.UseHsts();
+}
+
+App.UseHttpsRedirection();
+App.UseStaticFiles();
+App.UseRouting();
+App.MapBlazorHub();
+App.MapFallbackToPage("/_Host");
+App.MapControllers();
+
+App.Run();
