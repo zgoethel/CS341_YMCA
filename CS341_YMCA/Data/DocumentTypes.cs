7949aa54ce716694552b2d147b9b8ca31e0b0825
Zach Goethel <13502112+zgoethel@users.noreply.github.com>
2022-03-01 21:52:11 -0600

    Fixed issues with editor

--- CS341_YMCA/Data/DocumentTypes.cs
+++ CS341_YMCA/Data/DocumentTypes.cs
@@ -129,7 +129,7 @@
 
     public class ClassScheduleDBO
     {
-        public int Id { get; set; }
+        public int? Id { get; set; }
         public int ClassId { get; set; }
         public DateTime FirstDate { get; set; } = DateTime.Now;
         public int Recurrence { get; set; }
