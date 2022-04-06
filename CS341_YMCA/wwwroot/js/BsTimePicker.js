696276076a2544aa78deae27f58f0a0b6c882053
Zach Goethel <13502112+zgoethel@users.noreply.github.com>
2022-04-05 20:59:20 -0500

    Created timepicker component

---
+++ CS341_YMCA/wwwroot/js/BsTimePicker.js
@@ -0,0 +1,23 @@
+﻿var BsTimePicker =
+{
+    bindTimePicker: function (reference, id)
+    {
+        $(`#${id}`).datetimepicker(
+        {
+            format: 'hh:mm a'
+        }).click(function ()
+        {
+            $("*[data-action='incrementHours']").addClass("oi");
+            $("*[data-action='incrementHours']").addClass("oi-chevron-top");
+            $("*[data-action='decrementHours']").addClass("oi");
+            $("*[data-action='decrementHours']").addClass("oi-chevron-bottom");
+            $("*[data-action='incrementMinutes']").addClass("oi");
+            $("*[data-action='incrementMinutes']").addClass("oi-chevron-top");
+            $("*[data-action='decrementMinutes']").addClass("oi");
+            $("*[data-action='decrementMinutes']").addClass("oi-chevron-bottom");
+        }).on("dp.change", function (event)
+        {
+            reference.invokeMethodAsync("OnValueChanged", event.date);
+        });
+    }
+};
