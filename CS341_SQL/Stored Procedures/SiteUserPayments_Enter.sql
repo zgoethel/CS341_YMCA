36d196d2ce52e31c3907cf2bb17f7b8c7d66737a
Zach Goethel <13502112+zgoethel@users.noreply.github.com>
2022-04-08 10:22:28 -0500

    Added credit card validation to database proc

--- CS341_SQL/Stored Procedures/SiteUserPayments_Enter.sql
+++ CS341_SQL/Stored Procedures/SiteUserPayments_Enter.sql
@@ -17,6 +17,29 @@ BEGIN
     -- interfering with SELECT statements.
     SET NOCOUNT ON;
 
+	IF (GETDATE() > EOMONTH(@CardExpiry))
+	BEGIN
+		RAISERROR('It appears your card has expired; please provide another.', 18, 1);
+		RETURN;
+	END
+	-- Can't find if T-SQL supports better regular expressions
+	IF (@CardNumber NOT LIKE '[0-9][0-9][0-9][0-9][ -][0-9][0-9][0-9][0-9][ -][0-9][0-9][0-9][0-9][ -][0-9][0-9][0-9][0-9]'
+		AND @CardNumber NOT LIKE '[0-9][0-9][0-9][0-9][0-9][0-9][0-9][0-9][0-9][0-9][0-9][0-9][0-9][0-9][0-9][0-9]')
+	BEGIN
+		RAISERROR('Provided credit card should number be a series of 16 numerical digits.', 18, 1);
+		RETURN;
+	END
+	IF (@SecurityCode < 100 OR @SecurityCode > 999)
+	BEGIN
+		RAISERROR('CVC secuirty pins should be 3 numerical digits.', 18, 1);
+		RETURN;
+	END
+	IF (@PostalCode < 10000 OR @PostalCode > 99999)
+	BEGIN
+		RAISERROR('Billing postal code should be 5 numerical digits.', 18, 1);
+		RETURN;
+	END
+
     -- Insert the payment into the database and create ID
     INSERT INTO [SiteUserPayments]
     (
